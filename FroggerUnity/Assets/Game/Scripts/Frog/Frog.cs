0d98bee2564101aa46b2defa00cbf1250f47f756
mbpmm <marianomcr92@gmail.com>
2019-07-30 03:21:37 -0300

    Agregado un auto y movimiento hacia adelante de la rana

--- FroggerUnity/Assets/Game/Scripts/Frog/Frog.cs
+++ FroggerUnity/Assets/Game/Scripts/Frog/Frog.cs
@@ -6,22 +6,27 @@ public class Frog : MonoBehaviour
 {
     public FrogController frogAnim;
     // Start is called before the first frame update
+    private float t;
+    private Vector3 pos2;
     private float timer;
+    private float timer2;
     void Start()
     {
-
+        pos2 = transform.position;
         frogAnim = GetComponent<FrogController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        t += Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, pos2, t);
         timer += Time.deltaTime;
 
-        if (timer>2.5f)
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            frogAnim.Jump();
-            Invoke("Idle2",1.0f);
+            Move();
+            Invoke("Idle2",0.5f);
             timer = 0;
         }
 
@@ -32,5 +37,13 @@ public class Frog : MonoBehaviour
     void Idle2()
     {
         frogAnim.Idle();
+    }
+
+    public void Move()
+    {
+        frogAnim.Jump();
+        t = 0;
+        pos2 = transform.position + new Vector3(0, 0, 1);
+        
     }
 }
